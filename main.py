f8215d8bac99abd77e66ba76a2a9646b42f32126
Rednaxelus <alexander.soremba@web.de>
2019-12-24 00:12:13 +0000

    initial commit after around 8 hours of research and prototyping.
    (Code-)sources:
    
    Adrian Rosebrock:
    https://www.pyimagesearch.com/2017/01/02/rotate-images-correctly-with-opencv-and-python/
    
    Mousam Singh:
    re.search('(?<=Rotate: )\d+', newdata).group(0)
    
    Soroush:
    Image-sharpen-filter
    
    Acumenus:
    directory

---
+++ main.py
@@ -0,0 +1,67 @@
+import glob
+import pathlib
+import numpy as np
+import imutils
+import cv2
+import re
+import pytesseract
+import os
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Alex\AppData\Local\Tesseract-OCR\tesseract'
+
+imageFileEnding = "jpg"
+dirnameIn = ''
+dirnameOut = 'OUTPUT'
+
+pathlib.Path(dirnameOut).mkdir(parents=True, exist_ok=True)
+
+filePaths = dirnameIn + "*." + imageFileEnding
+
+if dirnameIn:
+    filePaths = dirnameIn + "/" + filePaths
+
+fileNames = []
+
+print(filePaths)
+
+images = []
+
+for file in glob.glob(filePaths):
+    images.append(cv2.imread(file, cv2.IMREAD_COLOR))
+    fileNames.append(file)
+
+print("how many images: " + str(len(images)))
+print(fileNames)
+
+
+def unsharp_mask(image2, kernel_size=(5, 5), sigma=1.0, amount=1.0, threshold=0):
+    """Return a sharpened version of the image, using an unsharp mask."""
+    blurred = cv2.GaussianBlur(image2, kernel_size, sigma)
+    sharpened = float(amount + 1) * image2 - float(amount) * blurred
+    sharpened = np.maximum(sharpened, np.zeros(sharpened.shape))
+    sharpened = np.minimum(sharpened, 255 * np.ones(sharpened.shape))
+    sharpened = sharpened.round().astype(np.uint8)
+    if threshold > 0:
+        low_contrast_mask = np.absolute(image2 - blurred) < threshold
+        np.copyto(sharpened, image2, where=low_contrast_mask)
+    return sharpened
+
+
+for image in images:
+    image = unsharp_mask(image)
+
+    newdata = pytesseract.image_to_osd(image, lang="equ+eng+deu")
+    print(newdata)
+    info = int(re.search('(?<=Orientation in degrees: )\d+', newdata).group(0))
+    print("----------------------", info)
+
+   # cv2.imshow("before", image)
+    #cv2.waitKey(0)
+
+    image = imutils.rotate_bound(image, info)
+
+    imageName = "result" + fileNames.pop(0)
+    cv2.imwrite(os.path.join(dirnameOut, imageName), image)
+
+   # cv2.imshow("Rotated (Correct)", image)
+    #cv2.waitKey(0)
